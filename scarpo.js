195b9d60e28a978072cd470cf9be702af1c96bc5
yettsyjk <yettsyjk@gmail.com>
2020-10-24 16:03:25 -0600

    javascript sketch issues

--- scarpo.js
+++ scarpo.js
@@ -6,7 +6,7 @@ function randomChoice(array){
     return array[Math.round( random(0, array.length - 1) ) ];
 }
 
-var infiniteRunner = Sketch.create({
+var InfiniteRunner = Sketch.create({
     fullscreen: false,
     width: 640,
     height: 360,
@@ -134,6 +134,7 @@ function PlatformManager(){
     this.colliding = false;
     this.platforms = [this.first, this.second, this.third];
 }
+
 PlatformManager.prototype.update = function(){
     this.first.x -= 3 + InfiniteRunner.acceleration;
     if (this.first.x + this.first.width < 0) {
@@ -165,4 +166,143 @@ if (this.third.x + this.third.width < 0 ){
     this.third.height = this.third.y + InfiniteRunner.height + 10;
     this.third.color = randomChoice(this.colors);
     }
+};
+
+PlatformManager.prototype.updateWhenLose = function(){
+    this.first.x = 300;
+    this.first.color = randomChoice(this.colors);
+    this.first.y = InfiniteRunner.width / random(2, 3);
+    this.second.x = (this.first.x + this.first.width) +
+    random(this.maxDistanceBetween - 150, this.maxDistanceBetween);
+    this.third.x = (this.second.x + this.second.width)+ random(
+        this.maxDistanceBetween - 150, this.maxDistanceBetween
+    );
+};
+
+function Particle(options){
+    this.x = options.x;
+    this.y = options.y;
+    this.size = 10;
+    this.velocityX = options.velocityX || random(-(InfiniteRunner.acceleration * 3)
+    + -8, -(InfiniteRunner.acceleration * 3));
+    this.velocityY = options.velocityY || random(-(
+        InfiniteRunner.acceleration * 3
+    ));
+    this.color = options.color;
+}
+
+Particle.prototype.draw = function(){
+    InfiniteRunner.fillStyle = this.color;
+    InfiniteRunner.fillRect(this.x, this.y, this.size, this,this.size);
+};
+
+InfiniteRunner.setup = function(){
+    this.jumpCount = 0;
+    this.acceleration = 0;
+    this.accelerationMaker = 0;
+    this.player = new Player({
+        x: 150, 
+        y: 30,
+        width: 32,
+        height: 32
+    });
+    this.platformManager = new PlatformManager();
+
+    this.particles =[];
+    this.particlesIndex = 0;
+    this.particlesMax = 20;
+    this.collidePlatform = null;
+    this.scoreColor = '#ed8a66';
+    this.jumpCountRecord = 0;
+};
+
+InfiniteRunner.update = function(){
+    this.player.update();
+    switch(this.jumpCount){
+        case 10:
+            this.accelerationMaker = 1;
+            this.platformManager.maxDistanceBetween = 430;
+            this.scoreColor = '#a067eb';
+            break;
+        case 25:
+            this.accelerationMaker = 2;
+            this.platformManager.maxDistanceBetween = 530;
+            this.scoreColor = '#4371a1';
+            break;
+        case 40:
+            this.accelerationMaker = 3;
+            this.platformManager.maxDistanceBetween = 580;
+            this.scoreColor = '#161618';
+            break;
+    }
+
+    this.acceleration += (this.accelerationMaker - this.acceleration) * 0.02;
+    for (i = 0; i < this.platformManager.platforms.length; i++){
+        if (this.player.intersects(this.platformManager.platforms[i] ) ){   
+            this.collidePlatform = this.platformManager.platforms[i];
+        }
+        if (this.player.y < this.platformManager.platforms[i].y){
+            this.player.y = this.platformManager.platforms[i].y;
+            this.player.velocityY = 0;
+        }
+        this.player.x = this.player.previousX;
+        this.player.y = this.player.previousY;
+        this.particles[(
+            this.particlesIndex++
+        )%this.particlesMax
+    ] = new Particle({
+        x: this.player.x,
+        y: this.player.y + this.player.height,
+        color: this.collidePlatform.color
+    });
+    if (this.player.intersectsLeft(this.platformManager.platforms[i])){
+        this.player.x = this.collidePlatform.x -64;
+        for (i = 0; i < 10; i++){
+            this.particles[(this.particlesIndex++)%this.particlesMax] = new Particle({
+                x: this.player.x + this.player.width,
+                y: random(this.player.y, this.player.y + this.player.height),
+                velocityY: random(-30, 30),
+                color: randomChoice([
+                    '#f39385', '#d2e2e1', this.collidePlatform.color
+                ])
+            });
+        };
+        this.player.velocityY = -10 + -(this.acceleration * 4);
+        this.player.velocityX = -20 + -(this.acceleration * 4);
+    } else {
+        if (this.dragging || this.keys.SPACE || this.keys.UP || this.keys.W){
+            this.player.velocityY = this.player.jumpSize;
+            this.jumpCount;
+            if (this.jumpCount > this.jumpCountRecord){
+                this.jumpCountRecord = this.jumpCount;
+            }
+        }
+    }
+    }
+
+
+    for (i =0; i < this.platformManager.platforms.length; i++){
+        this.platformManager.update();
+    };
+
+    for (i =0; i < this.particles.length; i++){
+        this.particles[i].update();
+    };
+};
+
+InfiniteRunner.draw = function(){
+    this.player.draw();
+    for (i =0; i <this.platformManager.platforms.length; i++){
+        this.platformManager.platforms[i].draw();
+    };
+    for (i = 0; i < this.particles.length; i++){
+        this.particles[i].draw();
+    };
+
+    this.font = '12pt Arial';
+    this.fillStyle = '#161618';
+    this.fillText('RECORD: '+this.jumpCountRecord, this.width - (150 + this.acceleration * 4)),
+    this.fillStyle = this.scoreColor;
+    this.font = (12 + (this.acceleration * 3)) + 'pt Arial';
+    this.fillText('JUMPS: ' +this.jumpCount, this.width - (150 + (this.acceleration * 4) ), 50);
 };
